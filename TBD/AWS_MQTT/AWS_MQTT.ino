57fa2a2fa6e414dacd29806d855d98f160e6838e
AldoVladimir <aldovladimir@ciencias.unam.mx>
2022-09-17 15:19:08 -0500

    Agregado Json parser a MQTT

--- TBD/AWS_MQTT/AWS_MQTT.ino
+++ TBD/AWS_MQTT/AWS_MQTT.ino
@@ -10,6 +10,7 @@
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <Adafruit_BMP280.h>
+#include <ArduinoJson.h>
 
 #include <Credentials.h>
 
@@ -20,6 +21,8 @@ const char* password = PASSWORD;
 //Servidor MQTT
 const char* mqtt_server = AWS_MQTT_SERVER;
 const int mqtt_port = 8883;
+String clientId = "Axolote_";
+
 
 String Read_rootca;
 String Read_cert;
@@ -27,11 +30,14 @@ String Read_privatekey;
 //********************************
 #define BUFFER_LEN  256
 long lastMsg = 0;
-char payload[BUFFER_LEN]; //Datos a escribir en SD y serial
-int value = 0;
+char payload[BUFFER_LEN]; //Datos a enviar por MQTT
 byte mac[6];
 char mac_Id[18];
-int count = 1;
+
+#define JSON_BUFFER_INCOMING_LEN 60
+#define JSON_BUFFER_OUTGOING_LEN 200
+DynamicJsonDocument payload_in(JSON_BUFFER_INCOMING_LEN);
+DynamicJsonDocument payload_out(JSON_BUFFER_OUTGOING_LEN);
 //********************************
 
 //Configuración de cliente MQTT
@@ -66,13 +72,21 @@ void setup_wifi() {
 
 //Callback
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Mensaje recibido [");
+
+  /*
+  Serial.print("Mensaje crudo recibido [");
   Serial.print(topic);
   Serial.print("] ");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
+  */
+  
+  //Json parser deserializer
+  deserializeJson(payload_in, payload);
+  const char* message = payload_in["message"];
+  Serial.println(message);
 }
 
 //Conectar a broker MQTT
@@ -80,21 +94,15 @@ void reconnect() {
   
   // Loop para reconección
   while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    
-    // Creando un ID como ramdon
-    String clientId = "ESP32-";
-    clientId += String(random(0xffff), HEX);
-    
+    Serial.print("Attempting MQTT connection...");  
+
     // Intentando conectarse
     if (client.connect(clientId.c_str())) {
-      Serial.println("conectada");
-      
-    // Conectado, publicando un payload...
-      client.publish("ei_out", "hello world");
-    
+      Serial.println("conectada");      
+   
     // ... y suscribiendo
-      client.subscribe("ei_in");
+      client.subscribe("$aws/things/Spiffs_test/sub");
+      
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -116,6 +124,7 @@ void setup() {
   pinMode(PIN_LED, OUTPUT);
   setup_wifi();
   delay(1000);
+  clientId += AXOLOTE_ID;
   
   //****************
   if (!SPIFFS.begin(true)) {
@@ -124,40 +133,46 @@ void setup() {
   }
   //**********************
   //Root CA leer archivo.
-  File file2 = SPIFFS.open("/AmazonRootCA1.pem", "r");
+  File file2 = SPIFFS.open("/AmazonRootCA1.pem", FILE_READ);
   if (!file2) {
     Serial.println("No se pudo abrir el archivo para leerlo");
     return;
   }
-  Serial.println("Root CA File Content:");
+  Serial.println("Leido Root CA File");
+  //Serial.println("Root CA File Content:");
   while (file2.available()) {
     Read_rootca = file2.readString();
-    Serial.println(Read_rootca);
-  }
+    //Serial.println(Read_rootca);
+    }
+    
   //*****************************
   // Cert leer archivo
-  File file4 = SPIFFS.open("/615f-certificate.pem.crt", "r");
+  File file4 = SPIFFS.open("/615f-certificate.pem.crt", FILE_READ);
   if (!file4) {
     Serial.println("No se pudo abrir el archivo para leerlo");
     return;
   }
-  Serial.println("Cert File Content:");
+  Serial.println("Leido Cert File");
+  //Serial.println("Cert File Content:");
   while (file4.available()) {
     Read_cert = file4.readString();
-    Serial.println(Read_cert);
-  }
+    //Serial.println(Read_cert);
+    }
+    
   //***************************************
   //Privatekey leer archivo
-  File file6 = SPIFFS.open("/615f-private.pem.key", "r");
+  File file6 = SPIFFS.open("/615f-private.pem.key", FILE_READ);
   if (!file6) {
     Serial.println("No se pudo abrir el archivo para leerlo");
     return;
   }
-  Serial.println("privateKey contenido:");
+  Serial.println("Leido privateKey");
+  //Serial.println("privateKey contenido:");
   while (file6.available()) {
     Read_privatekey = file6.readString();
-    Serial.println(Read_privatekey);
-  }
+    //Serial.println(Read_privatekey);
+    }
+  
   //=====================================================
 
   char* pRead_rootca;
@@ -171,7 +186,8 @@ void setup() {
   char* pRead_privatekey;
   pRead_privatekey = (char *)malloc(sizeof(char) * (Read_privatekey.length() + 1));
   strcpy(pRead_privatekey, Read_privatekey.c_str());
-
+  
+  /*
   Serial.println("================================================================================================");
   Serial.println("Certificados que pasan adjuntan al espClient");
   Serial.println();
@@ -186,7 +202,8 @@ void setup() {
   Serial.println("privateKey:");
   Serial.write(pRead_privatekey);
   Serial.println("================================================================================================");
-
+  */
+  
   espClient.setCACert(pRead_rootca);
   espClient.setCertificate(pRead_cert);
   espClient.setPrivateKey(pRead_privatekey);
@@ -198,7 +215,7 @@ void setup() {
   WiFi.macAddress(mac);
   snprintf(mac_Id, sizeof(mac_Id), "%02x:%02x:%02x:%02x:%02x:%02x",
            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-  Serial.println(mac_Id);
+  //Serial.println(mac_Id);
   //****************************************
   delay(2000);
  
@@ -206,15 +223,6 @@ void setup() {
 
 
 void loop() {
-
-  // ******************** SENSOR BMPx80 *********************************
-  float h = bmp.readPressure();         // Lectura de presion
-  float t = bmp.readTemperature();      // Lectura de humedad
-  if (isnan(h) || isnan(t))
-  {
-    Serial.println("¡Error al leer del sensor BMP!");
-    return;
-  }
 
   if (!client.connected()) {
     reconnect();
@@ -224,20 +232,29 @@ void loop() {
   long now = millis();
   if (now - lastMsg > 5000) {
     lastMsg = now;
-    //=============================================================================================
-    String macIdStr = mac_Id;
-    String Temprature = String(t);
-    String Humidity = String(h);
-    snprintf (payload, BUFFER_LEN, "{\"mac_Id\" : \"%s\", \"Temperatura\" : %s, \"Humedad\" : %s}", macIdStr.c_str(), Temprature.c_str(), Humidity.c_str());
+    //=============================================================================================    
+    /*snprintf(payload,BUFFER_LEN,
+                      "{\"mac_Id\" : \"%s\","
+                      "\"device_Id\" : \"%s\","
+                      "\"temp_C\" : \"%.2f\"," 
+                      "\"press_hPa\" : \"%.2f\"}", 
+                      mac_Id,
+                      clientId,                      
+                      bmp.readTemperature(),
+                      bmp.readPressure()/100);*/
+    //Json Serializer
+    payload_out["mac_Id"] = mac_Id;
+    payload_out["device_Id"] = clientId;
+    payload_out["temp_C"] = String(bmp.readTemperature(),2);
+    payload_out["press_hPa"] = String(bmp.readPressure()/100,2);
+
+    serializeJson(payload_out, payload);
+        
     Serial.print("Publicando mensaje: ");
-    Serial.print(count);
     Serial.println(payload);
-    client.publish("$aws/things/Spiffs_test", payload);
-    count = count + 1;
+
+    client.publish("$aws/things/Spiffs_test/pub", payload);
     //================================================================================================
   }
-  digitalWrite(2, HIGH);   
-  delay(1000);                      
-  digitalWrite(2, LOW);   
-  delay(1000);                       
+                      
 }
